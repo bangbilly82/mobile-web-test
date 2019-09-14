45472c0caf94af1a0c3a1c85dde5293580500547
Billy Rizky Juristra <billy@Billys-MacBook-Pro.local>
2019-09-14 23:01:44 +0700

    Update formatted code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import InputAmount from "./components/InputAmount";
+import React, { Component } from 'react';
+import InputAmount from './components/InputAmount';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="amount-parser__wrapper">
-        <div className="amount-parser__box">
+      <div className='amount-parser__wrapper'>
+        <div className='amount-parser__box'>
           <InputAmount />
         </div>
       </div>
